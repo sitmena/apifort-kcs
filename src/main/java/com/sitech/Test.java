db6d1b8b2690cd362e5aa813f05bcccbe0dd0296
Ajweh777 <87384312+Ajweh777@users.noreply.github.com>
2022-08-15 11:20:15 +0300

    update

---
+++ src/main/java/com/sitech/Test.java
@@ -0,0 +1,17 @@
+package com.sitech;
+
+import com.sitech.grpc.service.RealmGrpcService;
+import io.quarkus.runtime.StartupEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.event.Observes;
+
+public class Test {
+
+//    private static final Logger log = LoggerFactory.getLogger(Test.class);
+//    void onStart(@Observes StartupEvent ev) {
+//        log.info("The application is starting...");
+//        addUserRole("ajweh","ajweh","uma_authorization");
+//    }
+}
